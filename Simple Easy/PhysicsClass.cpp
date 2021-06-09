4228c52909bc01739ca9fb9025a76699f333262c
Mukesh Kumar Lodha <79532984+Mukesh01Coder@users.noreply.github.com>
2021-06-09 12:39:31 +0530

    PHYSICS

---
+++ Simple Easy/PhysicsClass.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#include <map>
+int main(){
+     ll t; cin >> t ;
+     while(t--){
+          ll n,f;
+          cin >>n>>f;
+          ll height[n];
+          map<ll, ll> mp;
+          for(ll i=0; i<n; i++){
+              cin >> height[i];
+              while (height[i]%f == 0)
+                   height[i]/=f; mp[height[i]]++;
+              
+          }
+          ll ways = 0;
+          for(auto it:mp){
+               ll sec = it.second;
+               ways+=(sec*(sec-1))/2;
+          }
+          cout<<ways<<'\n';
+     }
+     return 0;
+}

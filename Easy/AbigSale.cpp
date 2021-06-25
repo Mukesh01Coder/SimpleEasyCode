fcd210ef4bc59b0daf645f27625eff59f0c77cf2
Mukesh Kumar Lodha <79532984+Mukesh01Coder@users.noreply.github.com>
2021-06-25 14:22:15 +0530

    BIGSALE

---
+++ Easy/AbigSale.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+typedef long long ll;
+
+int main(){
+     ll t; cin>>t;
+     while(t--){
+        ll n; cin>>n;
+        double loss=0;
+        for(ll i=1;i<=n;i++){ 
+
+             ll p,q,d;
+             cin>>p>>q>>d;
+             double incp = p;  
+
+             incp += incp*d/100;
+             incp -=  incp*d/100;
+             
+             loss +=(p-incp)*q;
+                   
+        }
+       cout<<setprecision(30)<<loss<<endl;
+     }
+     return 0;
+}
